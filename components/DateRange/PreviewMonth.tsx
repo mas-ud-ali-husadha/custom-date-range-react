c64453250b3dc074d55649d812bcc69e51f708f4
Mas'ud Ali <masudali300402@gmail.com>
2025-07-01 19:07:08 +0700

    update

--- components/DateRange/PreviewMonth.tsx
+++ components/DateRange/PreviewMonth.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { format } from 'date-fns';
+
 const PreviewMonth = ({ day }: { day: Date }) => {
   return (
     <button
       type="button"
-      className="text-gray-300 relative z-0 flex w-full aspect-square transition duration-200 ease-in-out items-center justify-center text-sm font-medium rounded-lg hover:border"
+      className="text-gray-300 relative z-0 flex w-full aspect-square transition duration-200 ease-in-out items-center justify-center text-sm font-medium rounded-lg"
+      disabled
     >
       {format(day, 'd')}
     </button>
